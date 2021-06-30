4f0e61073148331dc5357fc0fa49dd2c53f167a6
Gustavo Nogueira <gustavo.nogueira@jobconvo.com>
2021-06-30 15:54:16 -0300

    First commit

--- backend/restapi/settings.py
+++ backend/restapi/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 INTERNAL_IPS = ['127.0.0.1']
 
