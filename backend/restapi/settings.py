ddb4f5f3c15c54645369fc49a3022816ce052877
Gustavo Nogueira <gustavo.nogueira@jobconvo.com>
2021-06-30 15:51:13 -0300

    First commit

--- backend/restapi/settings.py
+++ backend/restapi/settings.py
@@ -22,11 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY') #for aws
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 INTERNAL_IPS = ['127.0.0.1']
 
